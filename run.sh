f9326ee5e5a7ae97568dbc192881ab1dc514e712
Roxve <116169309+Roxve@users.noreply.github.com>
2024-07-07 23:31:00 +0300

    Revert "Hello, world!"
    This reverts commit 60cd55f932354f122dee0074c9e301fe181527f6.

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git clone https://github.com/novnc/noVNC --depth 1
+export qemu=qemu-system-i386
+$qemu -kernel kernel.bin -vnc :0 &
+
+echo "qemu starting..."
+sleep 2
+echo "done"
+
+cd noVNC
+echo "starting noVNC..."
+./utils/novnc_proxy --vnc localhost:5900 &
+sleep 2
+
+xdg-open http://localhost:6080/vnc.html &
+while true; do read -p "type exit to exit: " </dev/tty;  if [ "$REPLY" == "exit" ]; then break; fi; done
+killall $qemu
