d5804d67b655eb3cce766b13907ff966daa18d3a
Roxve <116169309+Roxve@users.noreply.github.com>
2024-07-07 23:33:09 +0300

    Revert "Hello, world!"
    This reverts commit 60cd55f932354f122dee0074c9e301fe181527f6.

--- src/kernel.c
+++ src/kernel.c
@@ -16,10 +16,19 @@ uint8_t vga_entry_color(VGA_COLOR fg, VGA_COLOR bg)
 	return fg | bg << 4;
 }
 // TERMINAL
+<<<<<<< HEAD
 
 // KERNEL
 void kernel_main() {
     while (1) {
         ;
     }
+=======
+uint16_t* terminal_buffer;
+
+// KERNEL
+void kernel_main() {
+	terminal_buffer = (uint16_t*) 0xB8000;
+    terminal_buffer[0] = vga_entry('H', vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK));
+>>>>>>> parent of 60cd55f (Hello, world!)
 }
