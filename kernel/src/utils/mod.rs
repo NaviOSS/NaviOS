f49111ab2ec13d7da95dd15a3b33a1a983a69cb1
Roxve <116169309+Roxve@users.noreply.github.com>
2024-08-28 10:56:23 +0300

    moved utils to a folder

---
+++ kernel/src/utils/mod.rs
@@ -0,0 +1,15 @@
+pub mod elf;
+// TODO: impl our own Optional type
+use spin::Mutex;
+
+pub struct Locked<T> {
+    pub inner: Mutex<T>,
+}
+
+impl<T> Locked<T> {
+    pub const fn new(inner: T) -> Self {
+        Self {
+            inner: Mutex::new(inner),
+        }
+    }
+}
