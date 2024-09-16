71d4f4dbd8fd31a10d13db6e059afc885dfad353
Roxve <116169309+Roxve@users.noreply.github.com>
2024-09-16 23:36:44 +0300

    divided build.rs into multiple functions, and added ramdisk building logic

--- build.rs
+++ build.rs
@@ -1,12 +1,15 @@
 // build.rs
 
-use std::{env::current_dir, fs, path::PathBuf, process::Command};
+use std::{
+    env::current_dir,
+    fs::{self, File},
+    path::PathBuf,
+    process::{Command, Output},
+};
 
-/// TODO: spilt into more functions and make it work on other oses like windows
-fn main() {
-    // set by cargo, build scripts should use this directory for output files
-    let kernel = PathBuf::from(std::env::var_os("CARGO_BIN_FILE_KERNEL_kernel").unwrap());
+use tar::Builder;
 
+fn limine_make() -> Output {
     if !fs::exists("limine").unwrap() {
         Command::new("git")
             .arg("clone")
@@ -21,18 +24,30 @@ fn main() {
         .arg("-C")
         .arg("limine")
         .output()
-        .unwrap();
+        .unwrap()
+}
 
+fn out(mut output: Output) {
+    output.stdout.append(&mut output.stderr);
+    eprintln!("{}", String::from_utf8_lossy(&output.stdout))
+}
+fn setup_iso_root() {
     fs::create_dir_all("iso_root/boot/limine").unwrap();
+    fs::create_dir_all("iso_root/EFI/BOOT").unwrap();
+}
 
-    Command::new("mv")
+fn put_kernel_img() {
+    let kernel = PathBuf::from(std::env::var_os("CARGO_BIN_FILE_KERNEL_kernel").unwrap());
+    out(Command::new("mv")
         .arg("-v")
         .arg(kernel)
         .arg("iso_root/boot/kernel")
         .output()
-        .unwrap();
+        .unwrap());
+}
 
-    Command::new("cp")
+fn put_limine_config() {
+    out(Command::new("cp")
         .arg("-v")
         .arg("limine.conf")
         .arg("limine/limine-bios.sys")
@@ -40,25 +55,28 @@ fn main() {
         .arg("limine/limine-uefi-cd.bin")
         .arg("iso_root/boot/limine")
         .output()
-        .unwrap();
+        .unwrap())
+}
 
-    fs::create_dir_all("iso_root/EFI/BOOT").unwrap();
-    Command::new("cp")
+fn put_boot_files() {
+    out(Command::new("cp")
         .arg("-v")
         .arg("limine/BOOTX64.EFI")
         .arg("iso_root/EFI/BOOT")
         .output()
-        .unwrap();
+        .unwrap());
 
-    Command::new("cp")
+    out(Command::new("cp")
         .arg("-v")
         .arg("limine/BOOTIA32.EFI")
         .arg("iso_root/EFI/BOOT")
         .output()
-        .unwrap();
+        .unwrap());
+}
 
+fn make_iso() {
     // command too long ):
-    Command::new("bash")
+    out(Command::new("bash")
         .arg("-c")
         .arg(
             "xorriso -as mkisofs -b boot/limine/limine-bios-cd.bin \
@@ -69,13 +87,47 @@ fn main() {
 ",
         )
         .output()
-        .unwrap();
+        .unwrap())
+}
 
+fn compile_programs() -> Output {
+    Command::new("make")
+        .arg("-C")
+        .arg("programs")
+        .output()
+        .unwrap()
+}
+
+fn make_ramdisk() {
+    let file = File::create("iso_root/boot/ramdisk.tar").unwrap();
+    let mut tar_builder = Builder::new(file);
+    tar_builder
+        .append_dir_all("programs", "programs/build")
+        .unwrap();
+}
+
+fn cleanup() {
     fs::remove_dir_all("iso_root").unwrap();
+}
+/// TODO: spilt into more functions and make it work on other oses like windows
+fn main() {
+    out(limine_make());
+    setup_iso_root();
+
+    put_kernel_img();
+    put_limine_config();
+    put_boot_files();
+
+    make_iso();
+    make_ramdisk();
+    out(compile_programs());
+    cleanup();
 
     let iso_path = current_dir().unwrap().join("navios.iso");
     println!("cargo:rerun-if-changed={}", iso_path.display());
     println!("cargo:rerun-if-changed={}", "limine");
+    println!("cargo:rerun-if-changed={}", "programs/build");
+    println!("cargo:rerun-if-changed={}", "programs");
 
     // pass the disk image paths as env variables to the `main.rs`
     println!("cargo:rustc-env=ISO_PATH={}", iso_path.display());
