45140fdb00b06104a5efe9c4f6c0933ac660e85e
Roxve <116169309+Roxve@users.noreply.github.com>
2024-10-10 23:05:40 +0300

    forgotten headergen

---
+++ libc/includes/sys/utils.h
@@ -0,0 +1,12 @@
+#ifndef __nlibc__SRC_SYS_UTILS_
+#define __nlibc__SRC_SYS_UTILS_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/types.h>
+
+#include "raw.h"
+SysInfo *sysinfo();
+
+#endif
