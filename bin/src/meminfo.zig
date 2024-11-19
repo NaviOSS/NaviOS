659710a6ac64a0828857f5218276eae21696549a
Roxve <116169309+Roxve@users.noreply.github.com>
2024-11-19 06:01:03 +0200

    sys:/bin/meminfo: added some new flags, and made use of %lu formating specifiers instead of %d

--- bin/src/meminfo.zig
+++ bin/src/meminfo.zig
@@ -1,16 +1,50 @@
 const libc = @import("libc");
 const sysinfo = libc.sys.utils.zsysinfo;
 const printf = libc.stdio.zprintf;
+
+const Mode = enum {
+    Bytes,
+    KiB,
+    MiB,
+    Verbose,
+};
 pub fn main() !void {
     const info = try sysinfo();
     const mem_ava: usize = info.total_mem - info.used_mem;
 
-    try printf("memory info:\n", .{});
-    try printf("%dB used of %dB, %dB usable\n", .{ info.used_mem, info.total_mem, mem_ava });
+    var mode: Mode = .Verbose;
+    var args = libc.sys.args();
 
-    try printf("%dKiBs used of %dKiBs, %dKiBs usable\n", .{ info.used_mem / 1024, info.total_mem / 1024, mem_ava / 1024 });
+    while (args.next()) |arg| {
+        if (libc.extra.eql(u8, arg, "-b")) {
+            mode = .Bytes;
+        } else if (libc.extra.eql(u8, arg, "-k")) {
+            mode = .KiB;
+        } else if (libc.extra.eql(u8, arg, "-m")) {
+            mode = .MiB;
+        }
+    }
 
-    try printf("%dMiBs used of %dMiBs, %dMiBs usable\n", .{ info.used_mem / 1024 / 1024, info.total_mem / 1024 / 1024, mem_ava / 1024 / 1024 });
+    switch (mode) {
+        .Bytes => {
+            try printf("%luB/%luB\n", .{ info.used_mem, info.total_mem });
+        },
+        .KiB => {
+            try printf("%luKiB/%luKiB\n", .{ info.used_mem / 1024, info.total_mem / 1024 });
+        },
+        .MiB => {
+            try printf("%luMiB/%luMiB\n", .{ info.used_mem / 1024 / 1024, info.total_mem / 1024 / 1024 });
+        },
+
+        .Verbose => {
+            try printf("memory info:\n", .{});
+            try printf("%luB used of %luB, %luB usable\n", .{ info.used_mem, info.total_mem, mem_ava });
+
+            try printf("%luKiBs used of %luKiBs, %luKiBs usable\n", .{ info.used_mem / 1024, info.total_mem / 1024, mem_ava / 1024 });
+
+            try printf("%luMiBs used of %luMiBs, %luMiBs usable\n", .{ info.used_mem / 1024 / 1024, info.total_mem / 1024 / 1024, mem_ava / 1024 / 1024 });
+        },
+    }
 }
 
 comptime {
